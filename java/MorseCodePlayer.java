0e4618bf2ab8e72e6f86642535ad3cc19c4dec4e
Islam <islamsakrak2002@gmail.com>
2020-12-11 08:46:58 +0100

    This MorseCodePlayer only contains the constructor from the set up which starts the program

---
+++ java/MorseCodePlayer.java
@@ -0,0 +1,7 @@
+public class MorseCodePlayer {
+    public static void main(String[] args) {
+        // initializing a start of the program, and making the main clan as clean as possible
+        // with using the constructor to start the program
+        new SetUp();
+    }
+} 
