dfeb1bd71dc7e646f6b60e85408bd9d0f29e44e8
IslamSakrak <islamsakrak2002@gmail.com>
2021-11-20 10:17:52 +0100

    Improving the name of the class and the methods and variables
    Removing unnecessary comments
    
    Splitting methods into multiple parts
    
    Changing loops from while to for
    
    Removing unnecessary things from morseCode map

--- java/LetterMatcher.java
+++ java/LetterMatcher.java
@@ -1,33 +1,26 @@
+import org.jetbrains.annotations.NotNull;
+
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
 
-public class SetUp extends Audio {
+public class LetterMatcher {
 
-    // creating input and text which gets translated to morse code
-    private final Scanner input = new Scanner(System.in);
-    private final String morse = input.nextLine().toUpperCase();
 
-// making a constructor that calls startAudio method which starts the program
-    public SetUp(){
-        startAudio();
-    }
-
-    // made of 2 keys methods, which loops trough the alphabet , and another method to initialize
-    public void startAudio() {
-        loopTroughSounds();
-    }
-    private void loopTroughSounds(){
-        int i = 0;
-        // while it loops trough the text , it goes trough the alphabet, and displays text in the console that matches each letter
-        while (i < morse.length()) {
-            letterPattern(morse, i);
-            // and gets called the initialization method which gives the path
-            getPathAndAudio(morse, i);
-            i++;
+    protected void loopTroughInput(){
+        String sampleMorseCodeInput = getSampleMorseCodeInput();
+        AudioInitialization audioInitialization = new AudioInitialization();
+        for (int i = 0; i < sampleMorseCodeInput.length(); i++){
+            letterToMorseCode(sampleMorseCodeInput, i);
+            audioInitialization.pathForAudio(sampleMorseCodeInput, i);
         }
     }
-    private void letterPattern(String morse, int index) {
+    @NotNull
+    private String getSampleMorseCodeInput() {
+        Scanner input = new Scanner(System.in);
+        return input.nextLine().toUpperCase();
+    }
+    private void letterToMorseCode(String morse, int index) {
         Map<Character, String> morseCode = new HashMap<>();
         morseCode.put('A', ".- ");
         morseCode.put('B', "-... ");
@@ -55,14 +48,7 @@ public class SetUp extends Audio {
         morseCode.put('X', "-..- ");
         morseCode.put('Y', "-.-- ");
         morseCode.put('Z', "--.. ");
-        morseCode.put(' ', " / ");
         // goes trough the key and values, filter the map for keys that match the index of the char, gets and prints the values
         morseCode.entrySet().stream().filter(map -> map.getKey().equals(morse.charAt(index))).map(Map.Entry::getValue).forEach(System.out::print);
     }
-    // sets up to sleep certain
-    public static void conditionSleep(int index) throws InterruptedException {
-        if (index < 3) Thread.sleep(650 * (2));
-        else Thread.sleep(580 * (2));
-    }
-
 }
