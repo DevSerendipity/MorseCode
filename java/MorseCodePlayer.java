1f3abf26e408111844d7541169785063e81302a5
IslamSakrak <islamsakrak2002@gmail.com>
2021-11-20 10:04:50 +0100

    Relocating files into a better location

--- java/MorseCodePlayer.java
+++
@@ -1,7 +0,0 @@
-public class MorseCodePlayer {
-    public static void main(String[] args) {
-        // initializing a start of the program, and making the main clan as clean as possible
-        // with using the constructor to start the program
-        new SetUp();
-    }
-} 
