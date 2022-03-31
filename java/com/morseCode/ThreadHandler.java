f0e733b90f1414477272a1eaea5ea3a359927178
Islam <islamsakrak2002@gmail.com>
2022-03-31 18:21:33 +0200

    Removing my comments

--- java/com/morseCode/ThreadHandler.java
+++ java/com/morseCode/ThreadHandler.java
@@ -3,7 +3,6 @@ package com.morseCode;
 public class ThreadHandler extends InputHandler{
     void sleepConditions() {
         try {
-            // Thread sleep based on word lenth
             Thread.sleep(1500);
         } catch (InterruptedException e) {
             e.printStackTrace();
